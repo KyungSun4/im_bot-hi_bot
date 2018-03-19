0bd6cf1e3a662f6f824bd25a88d2c25bd6cedf53
KyungSun4 <john.han7@yahoo.com>
2018-03-19 17:45:14 -0500

    added some more end words

--- hiimbot.py
+++ hiimbot.py
@@ -7,7 +7,7 @@ from time import sleep
 # A list of subreddits to check
 subreddits = ["funny", "videos", "pics", "gifs", "dankmemes", "hearthstone", "games", "memes"]
 
-endword = [".", ",", "and", "but", "so", "!", "?", "from"]
+endword = [".", ",", " and", " but", " so", "!", "?", " from", " as", " for", " nor", " or"]
 
 # Create the Reddit instance
 reddit = praw.Reddit('bot2')
